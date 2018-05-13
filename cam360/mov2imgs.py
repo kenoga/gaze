be3bcd298a731b290b7277997680e3ccdcf83adb
Kenjiro Nogawa <nogawa@pcl.cs.waseda.ac.jp>
2018-05-13 20:01:17 +0900

    change some files

--- cam360/mov2imgs.py
+++ cam360/mov2imgs.py
@@ -1,8 +1,7 @@
-
+# -*- coding: utf-8 -*-
 import os
 import numpy as np
 from io import BytesIO
-from skimage import io, draw
 import codecs
 import cv2
 
@@ -14,7 +13,8 @@ def extract_left(frame):
     width = frame.shape[1]
     return frame[:, 0:width//2]
 
-def mov2imgs(in_path, out_path, func):
+def mov2imgs(in_path, out_path, func=lambda x: return x, interval=2):
+#     print('test')
     # func: 動画から得られた画像に対してかける処理
     cap = cv2.VideoCapture(in_path)
     if not cap.isOpened():
@@ -24,8 +24,8 @@ def mov2imgs(in_path, out_path, func):
     frame_count = 0
     
     while True:
-        # 5フレームおきに保存する
-        for _ in range(0,4):
+        # interval+1フレームおきに保存する
+        for _ in range(0,interval):
             _, _ = cap.read()
         ret, frame = cap.read()
         frame_count += 5
@@ -33,11 +33,13 @@ def mov2imgs(in_path, out_path, func):
             break
         
         frame = func(frame)
-        img_name = ".".join([in_movie_name, '%08d' % frame_count, "jpg"])
-        cv2.imwrite(os.path.join(out_path, img_name), frame)
-        print(frame_count)
+#         img_name = "_".join([in_movie_name.split('.')[0],  "%08d" % fname_count]) + ".jpg"
+        print(in_movie_name + "->" + img_name)
+        
+#         cv2.imwrite(os.path.join(out_path, img_name), frame)
+#         print(frame_count)
     cap.release()
     
-in_path = '../data/movie/test_omni.mp4'
-out_dir = '../data/omni_image'
-mov2imgs(in_path, out_dir, extract_left)
+# in_path = '../data/movie/test_omni.mp4'
+# out_dir = '../data/omni_image'
+# mov2imgs(in_path, out_dir, extract_left)
--- cam360/polar.py
+++ cam360/polar.py
@@ -1,68 +1,70 @@
+# -*- coding: utf-8 -*-
+# ver 2.7
 
 import cv2
 import numpy as np
 import math
 import os
 
-def polar_transform(src):
-    # src = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
-
-    h_src, w_src, _ = src.shape
-
-    x_center, y_center = h_src // 2, w_src//2
-    y_center = w_src // 2
+class PolarTransformer():
     
-    # theta
-    w_dst = w_src * 2
-    # radius
-    h_dst = h_src // 2
+    def __init__(self, h_src, w_src):
+        self.h_src = h_src
+        self.w_src = w_src
+        
+        self.x_center, self.y_center = h_src // 2, w_src//2
+        
+        self.w_dst = w_src * 2
+        self.h_dst = h_src // 2
+        
+        # dst中のpxからsrc中のpxへのmappingをあらかじめ計算しておく
+        self.__create_polar_map()
     
-    dst = np.zeros((h_dst, w_dst, 3), np.uint8)
+    def __create_polar_map(self):
+        self.polar_map = [[0 for i in range(self.w_dst)] for j in range(self.h_dst)]
+        
+        for h_px in range(self.h_dst):
+            r = h_px
+            for w_px in range(self.w_dst):
+                t = 2 * math.pi * (float(w_px + 1) / self.w_dst)
+                x = int(r * math.cos(t))
+                y = -int(r * math.sin(t))
+                self.polar_map[h_px][w_px] = (y, x)
+      
+    def transform(self, src, h_top=0, h_bottom=1, w_left=0, w_right=1):
+        '''
+        height * h_topからheight * h_bottomまでが変換対象になる
+        width * w_leftからwidth * w_leftまでが変換対象になる
+        '''
+        assert h_top < h_bottom <= 1
+        assert w_left < w_right <= 1
+        dst = np.zeros((self.h_dst, self.w_dst, 3), np.uint8)
+        
+        top = int(self.h_dst * h_top)
+        bottom = int(self.h_dst * h_bottom)
+        left = int(self.w_dst * w_left)
+        right = int(self.w_dst * w_right)
+        for h_px in range(top, bottom):
+            for w_px in range(left, right):
+                src_y, src_x = self.polar_map[h_px][w_px]
+                dst[h_px][w_px] = src[self.y_center + src_y][self.x_center + src_x]
+        return dst[top:bottom, left:right]
 
-    for h_px in range(h_dst):
-        r = h_px
-        for w_px in range(w_dst):
-            t = 2 * math.pi * ((w_px + 1) / w_dst)
-            x = int(r * math.cos(t))
-            y = -int(r * math.sin(t))
-            dst[h_px][w_px] = src[y_center + y][x_center + x]
-    return dst
+    
+        
 
+if __name__ == '__main__':
+    src_dir = '../data/omni_image_test'
+    src_files = os.listdir(src_dir)
+    src_paths = [os.path.join(src_dir, src_file) for src_file in src_files if src_file[0] != '.']
+    
+    
 
-# # radius
-# w_dst = w_src // 2
-# # theta
-# h_dst = h_src
-# 
-# dst = np.zeros((h_dst, w_dst), np.uint8)
-# 
-# for h_px in range(h_dst):
-#     t = 2 * math.pi * ((h_px + 1) / h_dst)
-#     for w_px in range(w_dst):
-#         r = w_px
-#         x = int(r * math.cos(t))
-#         y = int(r * math.sin(t))
-#         dst[h_px][w_px] = src[y_center + y][x_center + x]
-# 
-# cv2.imwrite('../data/polar.jpg', dst)
-# 
-
-
-
-
-# src = cv2.imread('../data/test/testout.jpg')
-# dst = polar_transform(src)
-# cv2.imwrite('../data/polar.jpg', dst)
-
-src_dir = '../data/omni_image'
-src_files = os.listdir(src_dir)
-src_paths = [os.path.join(src_dir, src_file) for src_file in src_files if src_file[0] != '.']
-
-out_dir = '../data/para_image_polar'
-for path in src_paths:
-    print(path)
-    src = cv2.imread(path)
-    dst = polar_transform(src)
-    cv2.imwrite(os.path.basename(path), dst)
+    out_dir = '../data/para_image_polar_test'
+    for path in src_paths:
+        print(path)
+        src = cv2.imread(path)
+        dst = polar_transform(src)
+        cv2.imwrite(os.path.basename(path), dst)
 
 
