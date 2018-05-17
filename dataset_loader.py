b7cf348c98f427765c2d5bafa4126e57dd8990c6
Kenjiro Nogawa <noga-ken1107@toki.waseda.jp>
2018-05-17 14:53:38 +0900

    edit train codes for kobas-dataset

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,84 +1,103 @@
 # -*- coding: utf-8 -*-
 
 import os
+import random
 import glob
+
 from PIL import Image
 
-def separate_by_tag(paths):
-    locked = []
-    nonlocked = []
-    for path in paths:
-        fname = os.path.basename(path)
-        place = fname.split("_")[3]
-        if int(place) in {0, 1, 2, 31, 32}:
-            locked.append(path)
-        else:
-            nonlocked.append(path)
-    return locked, nonlocked
 
-def balance_size(a, b, random=False):
-    # TODO: randomに多いほうからサンプリングする
-    assert type(a) == type(b)
-    assert type(a) == list
+
+
+def paths2dict(paths, locked_targets):
+    # 0 or 1
+    def path2label(path, locked_targets):
+        target = int(path.split(".")[0].split("_")[3])
+        return int(target in path)
     
-    if len(a) == len(b):
-        return a, b
-    elif len(a) > len(b):
-        return a[:len(b)], b
-    else:
-        return a, b[:len(b)]
+    def path2pid(path):
+        return int(path.split(".")[0].split("_")[0])
+    
+    d = {}
+    for path in paths:
+        pid = path2pid(path)
+        label = path2label(path, locked_targets)
+        if pid not in d:
+            d[pid] = {}
+            d[pid][1] = []
+            d[pid][0] = []
+        d[pid][label].append(path)
+    return d
 
-def separate_train_and_test(paths, test_rate=0.8):
-    train = [path for path in paths[:int(len(locked)*(1-test_rate))]]
-    test = [path for path in paths[int(len(locked)*(test_rate)):]]
+def balance_locked_and_nonlocked(pid2paths, nonlocked_rate):
+    # TODO: randomに多いほうからサンプリングする
+    assert type(pid2paths) == dict
+    assert nonlocked_rate >= 1
+    
+    for pid, paths in pid2paths:
+        locked_paths = paths[0]
+        nonlocked_paths = paths[1]
+        paths[1] = ramdom.sample(nonlocked_paths, len(locked_paths) * nonlocked_rate)
+        
+    return pid2paths
+
+def separate_train_and_test(pid2paths, test_ids):
+    train = {}
+    test = {}
+    for pid, paths in pid2paths:
+        if pid in test_ids:
+            test[pid] = paths
+        else:
+            train[pid] = paths
+    
     return train, test
 
-def make_tuple_dataset(paths):
+def make_tuple_dataset(pid2paths):
     xs = []
     ts = []
     
-    for path in paths:
-        img = Image.open(path).convert('RGB') ## Gray->L, RGB->RGB
-        img = img.resize((INPUT_WIDTH, INPUT_HEIGHT))
+    for pid, paths in pid2paths:
+        for label, paths in paths.items():
+            for path in paths:
+                img = Image.open(path).convert('RGB') ## Gray->L, RGB->RGB
+                img = img.resize((INPUT_WIDTH, INPUT_HEIGHT))
 
-        x = np.array(img, dtype=np.float32)
-        ## Normalize [0, 255] -> [0, 1]
-        x = x / 255.
-        ## Reshape image to input shape of CNN
-        x = x.transpose(2, 0, 1)
-        #x = x.reshape(3, INPUT_HEIGHT, INPUT_WIDTH)
-
-        ## Get label(ground-truth) from file name path 
-        if '_0V_0H' in os.path.basename(path):
-            label = 0
-        else:
-            label = 1
-        t = np.array(label, dtype=np.int32)
-        
-        xs.append(x)
-        ts.append(t)
-    
+                x = np.array(img, dtype=np.float32)
+                ## Normalize [0, 255] -> [0, 1]
+                x = x / 255.
+                ## Reshape image to input shape of CNN
+                x = x.transpose(2, 0, 1)
+                #x = x.reshape(3, INPUT_HEIGHT, INPUT_WIDTH)
+                
+                t = np.array(label, dtype=np.int32)
+                
+                xs.append(x)
+                ts.append(t)
+            
     return np.array(xs).astype(np.float32), np.array(ts).astype(np.int32)
-        
-def load_dataset(dataset_dir, shuffle=True, test_rate=0.1):
-    assert test_rate < 1
-    assert os.exists(dataset_dir)
-    
-    filepaths = glob.glob(dataset_dir + '*/*.jpg')
-    assert len(filepaths) > 1000
-    
-    locked_vs_unlocked_rate = 1
-    locked, nonlocked = separate_by_tag(filepath)
-    locked, nonlocked = balance_size(locked, nonlocked, random=False)
-    assert len(locked) == len(nonlocked)
-    
-    train_locked, test_locked = separate_train_and_test()
-    train_nonlocked, test_nonlocked = separate_train_and_test()
-    assert len(train_locked+test_locked) == locked
-    assert len(train_nonlocked+test_nonlocked) = nonlocked
 
-    x_train, t_train = make_tuple_dataset(train_locked + train_unlocked)
-    x_test, t_test = make_tuple_dataset(test_locked + test_unlocked)
+
+def load_dataset(dataset_dir, test_ids, nonlocked_rate=1, locked_targets={0, 1, 2, 31, 32}):
+    assert os.exists(dataset_dir)
+    assert nonlocked_rate >= 1
+    
+    imgpaths = glob.glob(dataset_dir + '*/*.jpg')
+    assert len(imgpaths) > 1000
+    
+    locked_vs_nonlocked_rate = 1
+    
+    # person_id => (locked_paths, nonlocked_paths)のdictを作成
+    pid2paths = paths2dict(imgpaths, locked_targets)
+    
+    # 各人物におけるlocked_pathsとnonlocked_pathsの割合をランダムで調整する
+    pid2paths = balance_locked_and_nonlocked(pid2paths, nonlocked_rate)
+    
+    # 引数で与えられたtest_idsを元にdictをtrainとtestに分割する
+    train_pid2paths, test_pid2paths = separate_train_and_test(pid2paths, test_ids)
+    
+    # 画像を読み込んでndarrayのデータセットを作成する
+    x_train, t_train = make_dataset(train_pid2paths)
+    x_test, t_test = make_dataset(test_pid2paths)
     
     print('# Train locked images: {}'.format(len(train_locked_paths)))
     print('# Train unlocked images: {}'.format(len(train_unlocked_paths)))
