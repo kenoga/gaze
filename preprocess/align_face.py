0bda48a766843bee3dcc3b07b644a585b8e2262e
Kenjiro Nogawa <noga-ken1107@toki.waseda.jp>
2018-08-06 20:49:19 +0900

    edit align face

--- preprocess/align_face.py
+++ preprocess/align_face.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import os
+import openface
 
 module_path = os.path.abspath("..")
 if module_path not in sys.path:
     sys.path.append(module_path)
 
 from utils.PolarTransformer import PolarTransformer
-from dataset_utils.utils import get_bb_and_landmarks_dict, apply_func_against_srcdir_and_save_to_dstdir
+from utils.FaceAligner import FaceAligner
+from utils.utils import apply_func_against_srcdir_and_save_to_dstdir
+from dataset_utils.utils import get_bb_and_landmarks_dict
 from dataset_utils.config import DS_ROOT
 
 
@@ -17,18 +20,24 @@ def main():
     src_dir = os.path.join(DS_ROOT, 'transformed')
     dst_dir = os.path.join(DS_ROOT, 'aligned_face2')
 
-    def align_face(img, img_name, pid2bblms=None):
+    def align_face(img, img_name, pid2bblms=None, size=None, indices=None):
         assert pid2bblms is not None
+        assert size is not None
+        assert indices is not None
 
         pid = img_name.split("_")[0]
         if img_name in pid2bblms[pid] and pid2bblms[pid][img_name]['detected']:
             bb = pid2bblms[pid][img_name]['bb']
             bb_rec = dlib.rectangle(bb[0], bb[1], bb[2], bb[3])
             lms = pid2bblms[pid][img_name]['landmarks']
-            return  aligner.align(img=img, bb=bb_rec, landmarks=lms, size=96)
+            return  aligner.align(img=img, bb=bb_rec, landmarks=lms, size=size, indices=indices)
         return None
     
-    apply_func_against_srcdir_and_save_to_dstdir(src_dir, dst_dir, align_face, give_fname=True, pid2bblms=pid2bblms)
+    apply_func_against_srcdir_and_save_to_dstdir(src_dir, dst_dir, align_face, \
+                                                 give_fname=True, \
+                                                 pid2bblms=pid2bblms, \
+                                                 size=96, \
+                                                 indices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
     
     
 main()
--- preprocess/utils/FaceAligner.py
+++ preprocess/utils/FaceAligner.py
@@ -9,11 +9,13 @@ class FaceAligner():
     def __init__(self), dlib_model_path='/root/openface/models/dlib/shape_predictor_68_face_landmarks.dat'):
         self.aligner = AlignDlib(dlib_model_path)
         
-    def align(self, img_path=None, img=None, bb=None, landmarks=None, size=128):
-        if img_path is None and img is None:
-            raise RuntimeError("Please give a imgpath or a img.")
-        if img_path:
-            img = cv2.imread(img_path)
+    def align(self, img=None, bb=None, landmarks=None, size=None, indices=None):
+        assert img is not None
+        assert bb is not None
+        assert landmarks is not None
+        assert size is not None
+        assert indices is not None
+        
         return self.aligner.align(size, img, bb=bb, landmarks=landmarks)
     
     def bb(self, img_path=None, img=None):
