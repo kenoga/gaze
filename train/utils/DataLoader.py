8588e8b4620b9eb5a90c9bb05161be350f447843
Kenjiro Nogawa <nogaken1107@icloud.com>
2018-10-11 01:19:42 +0900

    fix

--- train/utils/DataLoader.py
+++ train/utils/DataLoader.py
@@ -72,8 +72,8 @@ class OmniEachEyeDataLoader(OmniDataLoader):
         l = np.array(left_eye, dtype=np.float32) / 255.0
         r = np.array(right_eye, dtype=np.float32) / 255.0
 
-        l = l.reshape(1, int(self.img_size[1]/2), self.img_size[1]) ## Reshape image to input shape of CNN
-        r = r.reshape(1, int(self.img_size[1]/2), self.img_size[1]) ## Reshape image to input shape of CNN
+        l = l.reshape(1, int(self.img_size[1]/2), self.img_size[0]) ## Reshape image to input shape of CNN
+        r = r.reshape(1, int(self.img_size[1]/2), self.img_size[0]) ## Reshape image to input shape of CNN
 
         t = np.array(int(path.locked), dtype=np.int32)
 
