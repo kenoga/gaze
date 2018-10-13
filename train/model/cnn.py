6a80d942cb6c41c2f42ab78142f42855cd3bbeeb
Kenjiro Nogawa <nogaken1107@icloud.com>
2018-10-13 14:40:49 +0900

    axis

--- train/model/cnn.py
+++ train/model/cnn.py
@@ -178,7 +178,7 @@ class CNNEachEyeWithAttention(chainer.Chain):
         eye1_h = eye1_h * ats
         eye2_h = eye2_h * ats
 
-        h = F.concat(eye1_h, eye2_h)
+        h = F.concat(eye1_h, eye2_h, axis=1)
 
         y = self.fc3(h)
         return y
