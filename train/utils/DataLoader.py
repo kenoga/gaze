251cd6c857126100145e9bbf1e0fc0cbf7fd565b
Kenjiro Nogawa <nogaken1107@icloud.com>
2018-10-13 14:34:02 +0900

    fix

--- train/utils/DataLoader.py
+++ train/utils/DataLoader.py
@@ -78,3 +78,39 @@ class OmniEachEyeDataLoader(OmniDataLoader):
         t = np.array(int(path.locked), dtype=np.int32)
 
         return l, r, t
+
+class OmniEachEyeWithFaceFeatureDataLoader(OmniDataLoader):
+    def __init__(self, img_size):
+        super(OmniEachEyeDataLoader, self).__init__(img_size)
+        pass
+
+    def load(self, path):
+        img = Image.open(path.path).convert('L') ## Gray->L, RGB->RGB
+        img = ImageOps.equalize(img)
+
+        img = img.resize((self.img_size[1], self.img_size[0]))
+
+        if path.mirror:
+            img = ImageOps.mirror(img)
+
+        left_eye = img.crop((0, 0, int(self.img_size[1]/2), self.img_size[0]))
+        right_eye = img.crop((int(self.img_size[1]/2), 0, self.img_size[1], self.img_size[0]))
+        right_eye = ImageOps.mirror(right_eye) # 反転させる
+
+        l = np.array(left_eye, dtype=np.float32) / 255.0
+        r = np.array(right_eye, dtype=np.float32) / 255.0
+
+        l = l.reshape(1, int(self.img_size[1]/2), self.img_size[0]) ## Reshape image to input shape of CNN
+        r = r.reshape(1, int(self.img_size[1]/2), self.img_size[0]) ## Reshape image to input shape of CNN
+
+        t = np.array(int(path.locked), dtype=np.int32)
+
+        f_list_nest = path.face_direction
+        if path.mirror:
+            # xを反転 (positionはx, yの順になっている
+            f_list_nest = [[1-position[0], position[1]]for position in f_list_nest]
+        f_list = [e / 255.0 for position in f_list_nest for e in position]
+        f = np.array(f_list, dtype=np.float32)
+        assert len(f) == 136
+
+        return l, r, f, t
