eb19cb8b4601bd06a87ef55c97a85367f742a538
Kenjiro Nogawa <nogaken1107@icloud.com>
2018-10-22 19:16:13 +0900

    add

--- train/model/cnn.py
+++ train/model/cnn.py
@@ -140,7 +140,7 @@ class CNNEachEye(chainer.Chain):
         return y
 
 
-class CNNEachEyeWithAttention(chainer.Chain):
+class CNNEachEyeWithAttention1(chainer.Chain):
 
     def __init__(self):
         super(CNNEachEyeWithAttention, self).__init__()
@@ -181,5 +181,46 @@ class CNNEachEyeWithAttention(chainer.Chain):
 
         h = F.concat((eye1_h, eye2_h), axis=1)
 
+        y = self.fc3(h)
+        return y
+
+class CNNEachEyeWithAttention2(chainer.Chain):
+
+    def __init__(self):
+        super(CNNEachEyeWithAttention, self).__init__()
+        with self.init_scope():
+            self.conv1_1 = L.Convolution2D(in_channels=None, out_channels=64, ksize=3, nobias=False)
+            self.conv1_2 = L.Convolution2D(in_channels=None, out_channels=128, ksize=3, nobias=False)
+            self.conv2_1 = L.Convolution2D(in_channels=None, out_channels=256, ksize=3, nobias=False)
+            self.conv2_2 = L.Convolution2D(in_channels=None, out_channels=128, ksize=3, nobias=False)
+            self.fc1 = L.Linear(None, 128, nobias=False)
+            self.fc2 = L.Linear(None, 32, nobias=False)
+            self.fc3 = L.Linear(None, 2, nobias=False)
+
+            self.fc_for_attention = L.Linear(136, 128, nobias=False)
+
+    def __call__(self, eye1, eye2, face):
+        h = F.relu(self.conv1_1(eye1))
+        h = F.relu(self.conv1_2(h))
+        h = F.max_pooling_2d(h, ksize=3)
+        h = F.relu(self.conv2_1(h))
+        h = F.relu(self.conv2_2(h))
+        eye1_h = F.max_pooling_2d(h, ksize=3)
+        eye1_h = F.dropout(F.relu(self.fc1(eye1_h)))
+
+        h = F.relu(self.conv1_1(eye2))
+        h = F.relu(self.conv1_2(h))
+        h = F.max_pooling_2d(h, ksize=3)
+        h = F.relu(self.conv2_1(h))
+        h = F.relu(self.conv2_2(h))
+        eye2_h = F.max_pooling_2d(h, ksize=3)
+        eye2_h = F.dropout(F.relu(self.fc1(eye2_h)))
+
+        ats = F.sigmoid(self.fc_for_attention(face))
+        eye1_h = eye1_h * ats
+        eye2_h = eye2_h * ats
+
+        h = F.concat((eye1_h, eye2_h), axis=1)
+        h = F.dropout(F.relu(self.fc2(h)))
         y = self.fc3(h)
         return y
