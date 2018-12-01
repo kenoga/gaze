2a6d917efd2fe5e88fc61a5f216e36d7b0f16b90
Kenjiro Nogawa <nogaken1107@icloud.com>
2018-12-01 17:12:25 +0900

    f

--- train/main.py
+++ train/main.py
@@ -14,6 +14,7 @@ import chainer
 import train
 from model.cnn import *
 from utils.DataPathProvider import DataPathProviderForCrossValidation
+from utils.DataPathProvider import DataPathProviderForTrain
 from utils.BatchProvider import BatchProvider
 from utils.DataLoader import *
 
